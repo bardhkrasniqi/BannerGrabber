b6cfa92170e98d8ad69b7b9a294523c2ac650cf5
root <root@localhost.localdomain>
2019-01-16 09:57:58 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='BannerGrabber',
+  packages=['BannerGrabber'],
+  version='1.0',
+  description='Simple python project that use sockets to get banner from services and check on local database if services are vulnerable ',
+  author='Bardh Krasniqi',
+  author_email='bardh.krasniqi11@gmail.com',
+  url='https://github.com/bardhkrasniqi/BannerGrabber', 
+  download_url= 'https://github.com/author/repo/tarball/1.0',
+  keywords = ['python', 'BannerGrabber'],
+  classifiers = [],
+)
